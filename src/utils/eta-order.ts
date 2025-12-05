d7bf9ec54905cb694870e115e7dfbb93df143f7a
felix <ffelixrichardo@gmail.com>
2025-12-06 01:54:02 +0700

    update eta order, add 10 minute

--- src/utils/eta-order.ts
+++ src/utils/eta-order.ts
@@ -1,7 +1,7 @@
 import { Order } from "@prisma/client";
 
 export function calculateETA(order: Order){
-    const eta = order.itemAmount * 10; // ETA in minutes
+    const eta = (order.itemAmount * 10) + 10; // ETA in minutes
     return {
         ...order,
         etaMinutes: eta
